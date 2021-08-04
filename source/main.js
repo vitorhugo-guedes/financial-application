cd5818d0193376970b5d8d5395eabde1ebecc502
vitorhugo-guedes <vitoroliveira.guedes@outlook.com>
2021-08-03 23:40:56 -0300

    Recode Update

--- source/main.js
+++ source/main.js
@@ -3,11 +3,14 @@ const closebtn = document.getElementById('closeBtn');
 const sidebar = document.getElementById('sidebar');
 const sidebarToggle = document.getElementById('sidebarToggle');
 
+const mainGrid = document.getElementById('main');
+
 sidebarToggle.addEventListener('click', () => {
-    sidebar.classList.toggle('sidebar-hidden');
+    sidebar.classList.toggle('show-sidebar');
+    mainGrid.classList.toggle('grid-test');
 })
 
 closebtn.addEventListener('click', () =>{
     const sidebarClass = sidebar.classList;
-    sidebarClass.remove('sidebar-hidden');
+    sidebarClass.remove('show-sidebar');
 })
